875614329f43e466c301c6fa3a063bea23deca6e
brianvlezb <brianvlezbl@gmail.com>
2026-04-01 10:21:29 -0500

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,14 +1,13 @@
 #include "VUMeter.h"
 
-// Standard COM-style DLL entry point required by VirtualDJ.
+// Standard COM-style DLL entry point required by VirtualDJ
 HRESULT VDJ_API DllGetClassObject(const GUID &rclsid, const GUID &riid, void **ppObject)
 {
-    // Usamos el CLSID y IID correctos para plugins DSP con el SDK público
     if (IsEqualGUID(rclsid, CLSID_VdjPlugin8) &&
         (IsEqualGUID(riid, IID_IVdjPlugin8) || IsEqualGUID(riid, IID_IVdjPluginDsp8)))
     {
         *ppObject = new CVUMeter();
-        return S_OK;                    // NO_ERROR es lo mismo que S_OK
+        return S_OK;
     }
 
     return CLASS_E_CLASSNOTAVAILABLE;
