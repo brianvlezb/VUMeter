5ae29f3092ec6b18383f16337ad08b7e330e014c
brianvlezb <brianvlezbl@gmail.com>
2026-04-01 10:28:05 -0500

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,7 +3,7 @@
 HRESULT VDJ_API DllGetClassObject(const GUID &rclsid, const GUID &riid, void **ppObject)
 {
     if (IsEqualGUID(rclsid, CLSID_VdjPlugin8) &&
-        (IsEqualGUID(riid, IID_IVdjPluginDsp8)))
+        IsEqualGUID(riid, IID_IVdjPluginDsp8))
     {
         *ppObject = new CVUMeter();
         return S_OK;
